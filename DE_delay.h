704c4f7169522f1977696d24265de825ab1c8dd7
DIY электроника <private.property240702@gmail.com>
2024-11-03 10:19:33 +0300

    Update DE_delay.h

--- DE_delay.h
+++ DE_delay.h
@@ -1,21 +1,36 @@
 #ifndef DE_delay_h
 #define DE_delay_h
 #include <Arduino.h>
+
+#define SECOND 1000
+
 class Delays {
   public:
-    uint16_t mill(uint16_t delMill) {
+    void mill(uint16_t delMill) {
       _delMill = delMill;
       _delMill = millis() + delMill;
       while (millis() < _delMill) {
       }
     }
-    uint32_t micr(uint32_t delMicr) {
+    void micr(uint32_t delMicr) {
       _delMicr = delMicr;
       _delMicr = micros() + delMicr;
       while (micros() < _delMicr) {
       }
     }
+    void blink(uint16_t period) {
+      _period = period;
+      while (millis() < period) {
+      }
+      digitalWtite(LED_BUILTIN, HIGH);
+
+      while (millis() < period) {
+      }
+      digitalWtite(LED_BUILTIN, LOW);
+    }
+
   private:
+    uint16_t _period;
     uint32_t _delMill;
     uint32_t _delMicr;
 };
